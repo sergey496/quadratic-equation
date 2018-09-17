0951eade4ec026f7d4f0b8d1ade88b4cba2a4fd2
Sergey Zylevich <sergey496@yandex.ru>
2018-09-17 15:41:29 +0300

    17.09

--- src/index.js
+++ src/index.js
@@ -8,19 +8,17 @@ module.exports = function solveEquation(equation) {
                 n=parseInt(n);
                 return n;
         }
-        var a= root(3);
-        var b= root(1);
-        var c= root(0);
-        var d=b*b-4*a*c;
-		var roots=[];
+        let a= root(3);
+        let b= root(1);
+        let c= root(0);
+        let d=b*b-4*a*c;
+		let roots=[];
         roots[0]=Math.round((-b+Math.sqrt(d))/(2*a));
         roots[1]=Math.round((-b-Math.sqrt(d))/(2*a));
 		if (roots[1]<roots[0]){
-			var k=roots[0];
+			let k=roots[0];
 			roots[0]=roots[1];
 			roots[1]=k;
 		}
-
-		
-		return roots;
+	return roots;
 }
